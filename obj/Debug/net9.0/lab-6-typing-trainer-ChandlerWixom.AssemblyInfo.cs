ad839279ca37f0594f3cc0c3bf378d39a2dc9322
Chandler Wixom <chandlerj.wixom@gmail.com>
2025-03-12 16:10:44 -0600

    missed Keys

--- obj/Debug/net9.0/lab-6-typing-trainer-ChandlerWixom.AssemblyInfo.cs
+++ obj/Debug/net9.0/lab-6-typing-trainer-ChandlerWixom.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-6-typing-trainer-ChandlerWixom")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59084d0580cf31398f196b8446981d7841d9cad6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3267c303bb94f5911b7f5a188c621f2143377e6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-6-typing-trainer-ChandlerWixom")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-6-typing-trainer-ChandlerWixom")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
