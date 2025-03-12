48cbb61391fba4ceb0cd1d212162f27c6fc7c3f8
Chandler Wixom <chandlerj.wixom@gmail.com>
2025-03-12 14:38:14 -0600

    Greatings

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// Chandler Wixom, 3/12/2025, Lab 6 Typing Trainer 
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.White;
+Console.Clear();
+Console.WriteLine("Hello, this program is a typing trainer.You will be given a random set of lines and your job is to type it out as fast and accuratly as posible.\n\tPress any key to start...");
+
